21b308aca70f801b122b68919550ca87b98ff718
Arjuna Sondhi <arjuna.sondhi@gmail.com>
2021-02-07 20:30:21 -0500

    Implementing Routing
    Implementing routing for login and welcome component
    
    Using react router to route from one component to another component

--- src/components/todo/TodoApp.jsx
+++ src/components/todo/TodoApp.jsx
@@ -1,12 +1,25 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 class TodoApp extends Component {
   render() {
     return (
       <div className="App">
-        <LoginComponent />
+        <Router>
+          <>
+            <Route path="/" exact component={LoginComponent} />
+            <Route path="/login" component={LoginComponent} />
+            <Route path="/welcome" component={WelcomeComponent} />
+          </>
+        </Router>
       </div>
     );
+  }
+}
+
+class WelcomeComponent extends Component {
+  render() {
+    return <div>Welcome Component</div>;
   }
 }
 
