9918b677761d68570fda04603351893446842fda
Arjuna Sondhi <arjuna.sondhi@gmail.com>
2021-02-07 20:14:25 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,4 @@
 import React, { Component } from "react";
-import FirstComponent from "./components/learning-examples/FirstComponent";
-import SecondComponent from "./components/learning-examples/SecondComponent";
-import ThirdComponent from "./components/learning-examples/ThirdComponent";
 import TodoApp from "./components/todo/TodoApp";
 // import logo from "./logo.svg";
 import "./App.css";
@@ -12,20 +9,6 @@ class App extends Component {
       <div className="App">
         {/* <Counter /> */}
         <TodoApp />
-      </div>
-    );
-  }
-}
-
-//Leanring Components example code
-class LearningComponents extends Component {
-  render() {
-    return (
-      <div className="learningComponents">
-        My Hello World
-        <FirstComponent />
-        <SecondComponent />
-        <ThirdComponent />
       </div>
     );
   }
