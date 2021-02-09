f75102d570f448b1fe8fc126993e0407fd691745
Arjuna Sondhi <arjuna.sondhi@gmail.com>
2021-02-09 17:19:26 -0500

    Header elements + LogoutComponents
    Added boostrap, created header html, added css to header, created LogoutComponent

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React, { Component } from "react";
 import TodoApp from "./components/todo/TodoApp";
-// import logo from "./logo.svg";
 import "./App.css";
-import "./bootstrap.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+// import "./bootstrap.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        {/* <Counter /> */}
         <TodoApp />
       </div>
     );
--- src/components/todo/TodoApp.jsx
+++ src/components/todo/TodoApp.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 import { ListTodosComponent } from "./ListTodosComponent";
 import { LoginComponent } from "./LoginComponent";
 
@@ -15,6 +15,7 @@ class TodoApp extends Component {
               <Route path="/login" component={LoginComponent} />
               {/* <Route path="/welcome/:name" component={ListTodosComponent} /> */}
               <Route path="/todos/:name" component={ListTodosComponent} />
+              <Route path="/logout" component={LogoutComponent} />
               <Route component={ErrorComponent} />
             </Switch>
             <FooterComponent />
@@ -28,9 +29,39 @@ class TodoApp extends Component {
 class HeaderComponent extends Component {
   render() {
     return (
-      <div>
-        Header <hr />
-      </div>
+      <header>
+        <nav className="navbar navbar-expand-md navbar-dark bg-dark">
+          <div>
+            <a href="" className="navbar-brand">
+              admin
+            </a>
+          </div>
+          <ul className="navbar-nav">
+            <li>
+              <Link className="nav-link" to="/todos/admin">
+                Home
+              </Link>
+            </li>
+            {/* <li>
+              <Link className="nav-link" to="/todos">
+                Todos
+              </Link>
+            </li> */}
+          </ul>
+          <ul className="navbar-nav navbar-collapse justify-content-end">
+            <li>
+              <Link className="nav-link" to="/login">
+                Login
+              </Link>
+            </li>
+            <li>
+              <Link className="nav-link" to="/logout">
+                Logout
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
     );
   }
 }
@@ -42,6 +73,16 @@ class FooterComponent extends Component {
         <hr />
         Footer
       </div>
+    );
+  }
+}
+
+class LogoutComponent extends Component {
+  render() {
+    return (
+      <>
+        <h1>Logged out successfully!</h1>
+      </>
     );
   }
 }
